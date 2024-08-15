c8b1c630e644e922092073a8463a29d6ddd849b3
Ian Becker <ian.becker@he360.com>
2024-08-15 19:44:48 -0400

    feat(terminal): add binary release from latest 1.45.0 glab cli

--- terminal-apps/default.nix
+++ terminal-apps/default.nix
@@ -1,4 +1,18 @@
 { flake, pkgs, ... }:
+let
+  gitlabCli = pkgs.stdenv.mkDerivation {
+    pname = "gitlab-cli";
+    version = "1.45.0";
+    src = builtins.fetchTarball {
+      url = "https://gitlab.com/gitlab-org/cli/-/package_files/140883199/download";
+      sha256 = "16ayn4k5gbzl6909vcfc4wqx66hx0xngndj6b3a9phc0wsj9gjcx";
+    };
+    installPhase = ''
+      mkdir -p $out/bin
+      install -m755 glab $out/bin/glab
+    '';
+  };
+in
 {
     imports = [
       ./awscli.nix
@@ -22,7 +36,8 @@
       
       gpg-tui
       
-      glab # gitlab cli
+      # glab # gitlab cli, hasn't been updated in a while...
+      gitlabCli # the fetchTarball above is from the latest release
       jira-cli-go
 
       podman
