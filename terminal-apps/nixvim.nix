e18d718c64aa2e5747a3ef08b1698807ed80045c
Ian Becker <ian.becker@he360.com>
2024-08-23 17:33:25 -0400

    feat(terminal): add initial nixvim config
    Signed-off-by: Ian Becker <ian.becker@he360.com>

--- terminal-apps/nixvim.nix
+++ terminal-apps/nixvim.nix
@@ -1,6 +1,52 @@
 { flake, pkgs, ... }:
 {
+  imports = [
+    flake.inputs.nixvim.homeManagerModules.nixvim
+  ];
+
   programs.nixvim = {
     enable = true;
+    colorschemes = {
+      catppuccin = {
+        enable = true;
+        settings = {
+          flavour = "macchiato";
+          integrations = {
+            cmp = true;
+            gitsigns = true;
+            mini = {
+              enabled = true;
+              indentscope_color = "";
+            };
+            notify = false;
+            nvimtree = true;
+            treesitter = true;
+          };
+        };
+      };
+    };
+    clipboard = {
+      providers.xsel.enable = true;
+    };
+
+    plugins = {
+      direnv.enable = true;
+      telescope.enable = true;
+      neo-tree = {
+        enable = true;
+        enableGitStatus = true;
+        enableModifiedMarkers = true;
+        enableRefreshOnWrite = true;
+      };
+    };
+
+    keymaps = [
+      {
+        mode = [ "n" ];
+        key = "<leader>e";
+        action = "<cmd>Neotree toggle<cr>";
+        options = { desc = "Open/Close Neotree"; };
+      }
+    ];
   };
 }
